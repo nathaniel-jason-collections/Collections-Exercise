5c697825dcf7493e899a72664ce9bb832f2703f6
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-05-23 13:29:14 -0500

    lunchtime commit

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -12,4 +12,31 @@ public class Student {
         this.name = name;
         this.grades = new ArrayList<>();
     }
+
+    //methods
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void addGrade(int grade) {
+        grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        int sum = 0;
+        for (int i=0; i<grades.size(); i++) {
+            sum += grades.get(i);
+        }
+        return sum/grades.size();
+    }
+
+    public static void main(String[] args) {
+        Student nathaniel = new Student("Nathnaiel Dowell", new ArrayList<>());
+        nathaniel.addGrade(84);
+        nathaniel.addGrade(97);
+        nathaniel.addGrade(100);
+        nathaniel.addGrade(97);
+        System.out.println("nathaniel.getGradeAverage() = " + nathaniel.getGradeAverage());
+    }
 }
